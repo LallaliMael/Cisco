c4e47c5dd7e7a9c7035bf5fb6ff63f93df6da7d9
LallaliMael <33594230+LallaliMael@users.noreply.github.com>
2017-11-30 17:38:59 +0100

    Update parole.py

--- Cisco/parole.py
+++ Cisco/parole.py
@@ -5,26 +5,17 @@ from gtts import gTTS
 
 nombre = 1
 chemin = str(str(nombre) + ".mp3")
-chemin_vers_dossier_synthese_vocale = "chemin_vers_votre_dossier_synthese_vocale"
-chemine = ''
-chemine = (chemin_vers_dossier_synthese_vocale + chemin)
-nombre = 1
-
+global nombre
+global chemin
 
 def dit(lire):
  print(chemin)
  tts = gTTS(text=lire, lang='fr')
- tts.save(chemine)
+ tts.save(chemin)
  pygame.mixer.init()
- pygame.mixer.music.load(chemine)
+ pygame.mixer.music.load(chemin)
  nombre += 1
  pygame.mixer.music.play()
  while pygame.mixer.music.get_busy():
               pass 
- global nombre
- global chemin
- global chemine
- global chemin_vers_dossier_synthese_vocale
  chemin = str(str(nombre) + ".mp3")
- chemine = ''
- chemine = (chemin_vers_dossier_synthese_vocale + chemin)
